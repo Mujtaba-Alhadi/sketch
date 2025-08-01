c8ad302ae0790a38eaa270f417ac10a39101ada6
mujtaba-alhadi <mujtabaalhadi22@gmail.com>
2025-08-01 19:38:16 +0300

    added features: [rainbow mode, color input]

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
 const container = document.querySelector(".container");
-const resize = document.querySelector(".resize");
+const resizeBtn = document.querySelector(".resize");
+const rainbowBtn = document.querySelector(".rainbow");
+const colorInput = document.querySelector(".colorInput");
+
+function generateRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  return `rgb(${red} ${green} ${blue})`;
+}
+
+rainbowBtn.addEventListener("click", () => {
+  rainbowBtn.classList.toggle("active");
+});
 
 function generateGrid(gridSize) {
   container.innerHTML = ""; // clear previous grid
+  const boxSize = 400 / gridSize;
   for (let i = 0; i < gridSize * gridSize; i++) {
     const box = document.createElement("div");
     box.className = "box";
-    box.style.flex = `${100 / gridSize}%`;
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
     box.addEventListener("mouseenter", () => {
-      box.style.backgroundColor = "#333";
+      if (rainbowBtn.classList[1] === "active") {
+        box.style.backgroundColor = generateRandomColor();
+      } else {
+        box.style.backgroundColor = colorInput.value;
+      }
     });
+    
     container.appendChild(box);
   }
 }
+
 generateGrid(16);
 
-resize.addEventListener("click", () => {
+resizeBtn.addEventListener("click", () => {
   let input = prompt("Enter a Number for The New Grid [1-100]");
   if (input === null) {
     return;
